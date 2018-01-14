044baf27dbe4450c5d84dd577df59f3df27dad30
Ronalson Filho <contact@ronalson.com.br>
2018-01-14 20:48:14 -0200

    Implement fizzBuzz

---
+++ src/fizz-buzz.js
@@ -0,0 +1,10 @@
+const fizzBuzz = (number) => {
+  if (number === 0) return 0
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz'
+  if (number % 3 === 0) return 'Fizz'
+  if (number % 5 === 0) return 'Buzz'
+
+  return number
+}
+
+export default fizzBuzz
--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -7,5 +7,37 @@ Escreva uma lib que receba um número e:
 - Se o número for divisível por 5, print no console 'Buzz'
 - Se o número for divisível por 3 e 5, print no console 'FizzBuzz'
 - Se não for multiplo de 3 e 5, print o número
+- Se o número for 0, print 0
 
 */
+
+import { expect } from 'chai'
+import fizzBuzz from './../src/fizz-buzz'
+
+describe('FizzBuzz', () => {
+  it('FizzBuzz should exist', () => {
+    expect(fizzBuzz).to.exist
+    expect(fizzBuzz).to.be.a('function')
+  })
+
+  it('should return `Fizz` if multiple of 3', () => {
+    expect(fizzBuzz(3)).to.be.equal('Fizz')
+    expect(fizzBuzz(6)).to.be.equal('Fizz')
+  })
+
+  it('should return `Buzz` if multiple of 5', () => {
+    expect(fizzBuzz(5)).to.be.equal('Buzz')
+  })
+
+  it('should return `FizzBuzz` if multiple of 3 & 5', () => {
+    expect(fizzBuzz(15)).to.be.equal('FizzBuzz')
+  })
+
+  it('should return number if not multiple of 3 or 5', () => {
+    expect(fizzBuzz(7)).to.be.equal(7)
+  })
+
+  it('should return 0 if 0', () => {
+    expect(fizzBuzz(0)).to.be.equal(0)
+  })
+})
