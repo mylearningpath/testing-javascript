b531c098230f5025e3ded459c37a3e00557bfd12
Ronalson Filho <contact@ronalson.com.br>
2018-01-14 17:56:45 -0200

    Add specs for FizzBuzz challenge

---
+++ test/fizzbuzz.spec.js
@@ -0,0 +1,11 @@
+/*
+Desafio FIZZBUZZ
+
+Escreva uma lib que receba um número e:
+
+- Se o número for divisível por 3, print no console 'Fizz'
+- Se o número for divisível por 5, print no console 'Buzz'
+- Se o número for divisível por 3 e 5, print no console 'FizzBuzz'
+- Se não for multiplo de 3 e 5, print o número
+
+*/
