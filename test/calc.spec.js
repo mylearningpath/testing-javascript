a89137526495e88fa44a2f892d84edaa69d8350f
Ronalson Filho <contact@ronalson.com.br>
2018-01-14 17:50:59 -0200

    Refactor calc lib to use ES6 syntax

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -1,62 +1,58 @@
-const expect = require('chai').expect
-const calc = require('./../src/calc')
+import { expect } from 'chai'
+import { sum, sub, mult, div } from './../src/calc'
 
-describe('Calc', function () {
-  describe('Smoke tests', function () {
-    it('should exist calc lib', function () {
-      expect(calc).to.exist
+describe('Calc', () => {
+  describe('Smoke tests', () => {
+    it('should exist sum method', () => {
+      expect(sum).to.exist
+      expect(sum).to.be.a('function')
     })
 
-    it('should exist sum method', function () {
-      expect(calc.sum).to.exist
-      expect(calc.sum).to.be.a('function')
+    it('should exist sub method', () => {
+      expect(sub).to.exist
+      expect(sub).to.be.a('function')
     })
 
-    it('should exist sub method', function () {
-      expect(calc.sub).to.exist
-      expect(calc.sub).to.be.a('function')
+    it('should exist mult method', () => {
+      expect(mult).to.exist
+      expect(mult).to.be.a('function')
     })
 
-    it('should exist mult method', function () {
-      expect(calc.mult).to.exist
-      expect(calc.mult).to.be.a('function')
-    })
-
-    it('should exist div method', function () {
-      expect(calc.div).to.exist
-      expect(calc.div).to.be.a('function')
+    it('should exist div method', () => {
+      expect(div).to.exist
+      expect(div).to.be.a('function')
     })
   })
 
-  describe('Sum', function () {
-    it('should return 4 when sum(2,2)', function () {
-      expect(calc.sum(2, 2)).to.equal(4)
+  describe('Sum', () => {
+    it('should return 4 when sum(2,2)', () => {
+      expect(sum(2, 2)).to.equal(4)
     })
   })
 
-  describe('Sub', function () {
-    it('should return 4 when sub(6,2)', function () {
-      expect(calc.sub(6, 2)).to.equal(4)
+  describe('Sub', () => {
+    it('should return 4 when sub(6,2)', () => {
+      expect(sub(6, 2)).to.equal(4)
     })
 
-    it('should return -2 when sub(2,4)', function () {
-      expect(calc.sub(2, 4)).to.equal(-2)
-    })
-  })
-
-  describe('Mult', function () {
-    it('should return 10 when mult(5, 2)', function () {
-      expect(calc.mult(5, 2)).to.equal(10)
+    it('should return -2 when sub(2,4)', () => {
+      expect(sub(2, 4)).to.equal(-2)
     })
   })
 
-  describe('Div', function () {
-    it('should return 2 when div(8, 4)', function () {
-      expect(calc.div(8, 4)).to.equal(2)
+  describe('Mult', () => {
+    it('should return 10 when mult(5, 2)', () => {
+      expect(mult(5, 2)).to.equal(10)
+    })
+  })
+
+  describe('Div', () => {
+    it('should return 2 when div(8, 4)', () => {
+      expect(div(8, 4)).to.equal(2)
     })
 
-    it('should return `Can\'t divide by 0` when div(4, 0)', function () {
-      expect(calc.div(4, 0)).to.equal('Can\'t divide by 0')
+    it('should return `Can\'t divide by 0` when div(4, 0)', () => {
+      expect(div(4, 0)).to.equal('Can\'t divide by 0')
     })
   })
 })
