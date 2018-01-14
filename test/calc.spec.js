7ef2af81f444dc52c91fa7162b2b640126488e2f
Ronalson Filho <contact@ronalson.com.br>
2018-01-14 17:40:52 -0200

    Add simple calculation functionality

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -27,4 +27,36 @@ describe('Calc', function () {
       expect(calc.div).to.be.a('function')
     })
   })
+
+  describe('Sum', function () {
+    it('should return 4 when sum(2,2)', function () {
+      expect(calc.sum(2, 2)).to.equal(4)
+    })
+  })
+
+  describe('Sub', function () {
+    it('should return 4 when sub(6,2)', function () {
+      expect(calc.sub(6, 2)).to.equal(4)
+    })
+
+    it('should return -2 when sub(2,4)', function () {
+      expect(calc.sub(2, 4)).to.equal(-2)
+    })
+  })
+
+  describe('Mult', function () {
+    it('should return 10 when mult(5, 2)', function () {
+      expect(calc.mult(5, 2)).to.equal(10)
+    })
+  })
+
+  describe('Div', function () {
+    it('should return 2 when div(8, 4)', function () {
+      expect(calc.div(8, 4)).to.equal(2)
+    })
+
+    it('should return `Can\'t divide by 0` when div(4, 0)', function () {
+      expect(calc.div(4, 0)).to.equal('Can\'t divide by 0')
+    })
+  })
 })
